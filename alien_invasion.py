c1b956461b1b9c1125255da07717bbea1dd792a2
Jia <jia_du@hotmail.com>
2020-11-17 21:09:33 -0500

    Add movements to the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,15 +15,13 @@ class AlienInvasion:
 			self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption('Alien Invasion')
 
-		#Set the background color.
-		self.bg_color = (230, 230, 230)
-
 		self.ship = Ship(self)
 
 	def run_game(self):
 		'''Start the main loop for the game.'''
 		while True:
 			self._check_events()
+			self.ship.update()
 			self._update_screen()
 
 	def _check_events(self):
@@ -31,6 +29,29 @@ class AlienInvasion:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				self._check_keydown_event(event)
+			elif event.type ==pygame.KEYUP:
+				self._check_keyup_event(event)
+
+
+
+	def _check_keydown_event(self, event):
+		'''Respond to key pressess'''
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = True
+		elif event.key == pygame.K_q:
+				sys.exit()
+
+
+	def _check_keyup_event(self, event):
+		'''Respond to key presses'''
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
 
 	def _update_screen(self):
 		'''Update images on the screen, and flip to the new screen.'''
@@ -39,6 +60,7 @@ class AlienInvasion:
 
 		#Make the most recently drawn screen visible
 		pygame.display.flip()
+
 
 
 if __name__ =='__main__':
